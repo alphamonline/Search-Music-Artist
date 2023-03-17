dd9988680a93153b5e8d91f5d26af7e59392a87f
Alpha M <105058020+alphamonline@users.noreply.github.com>
2023-03-17 15:39:16 +0200

    Vue routes, login and registration. Home, Albums, Artists, Songs page and auth checks

---
+++ vue/src/router/index.js
@@ -0,0 +1,53 @@
+import {createRouter, createWebHistory} from "vue-router";
+import Home from "../views/dashboard/Home.vue";
+import Artists from "../views/dashboard/Artists.vue";
+import Albums from "../views/dashboard/Albums.vue";
+import Songs from "../views/dashboard/Songs.vue";
+import Login from "../views/auth/Login.vue";
+import Register from "../views/auth/Register.vue";
+import DefaultLayout from "../components/DefaultLayout.vue";
+import AuthLayout from "../components/AuthLayout.vue";
+import store from "../store/index.js";
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/home',
+    component: DefaultLayout,
+    meta: {requiresAuth: true},
+    children: [
+      {path: '/home', name: 'Home', component: Home},
+      {path: '/artists', name: 'Artists', component: Artists},
+      {path: '/albums', name: 'Albums', component: Albums},
+      {path: '/songs', name: 'Songs', component: Songs},
+    ]
+  },
+  {
+    path: '/auth',
+    redirect: '/login',
+    name: 'Auth',
+    component: AuthLayout,
+    meta: {isGuest: true},
+    children: [
+      {path: '/login', name: 'Login', component: Login},
+      {path: '/register', name: 'Register', component: Register}
+    ]
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.user.token) {
+    next({name: 'Login'})
+  } else if (store.state.user.token && (to.meta.isGuest)) {
+    next({name: 'Home'});
+  } else {
+    next();
+  }
+})
+
+export default router;
