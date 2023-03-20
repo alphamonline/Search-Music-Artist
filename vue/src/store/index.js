82c5b1b34a3e3edad3fd00e2bc9c271ab3bcfe00
Alpha M <105058020+alphamonline@users.noreply.github.com>
2023-03-20 11:05:59 +0200

    Finalised Sanctum and vue with axios

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,25 +1,60 @@
 import {createStore} from "vuex";
+import axiosClient from "../axios";
 
 const store = createStore({
   state: {
     user: {
-      data: {
-        user: {
-          name: 'Alpha Matumbura',
-          email: 'alphamonline@gmail.com',
-          imageUrl: 'https://picsum.photos/150',
-        }
-      },
-      token: 123
+      data: {},
+      token: sessionStorage.getItem("TOKEN"),
     }
   },
   getters: {},
-  actions: {},
+  actions: {
+    register({commit}, user) {
+      return axiosClient.post('/register', user)
+        .then(({data}) => {
+          commit('setUser', data.user);
+          commit('setToken', data.token)
+          return data;
+        })
+    },
+    login({commit}, user) {
+      return axiosClient.post('/login', user)
+        .then(({data}) => {
+          commit('setUser', data.user);
+          commit('setToken', data.token)
+          return data;
+        })
+    },
+    logout({commit}) {
+      return axiosClient.post('/logout')
+        .then(response => {
+          commit('logout')
+          return response;
+        })
+    },
+    getUser({commit}) {
+      return axiosClient.get('/user')
+        .then(res => {
+          console.log(res);
+          commit('setUser', res.data)
+        })
+    },
+  },
   mutations: {
-    logout: state => {
+    logout: (state) => {
+      state.user.token = null;
       state.user.data = {};
-        state.user.token = null;
-    }
+      sessionStorage.removeItem("TOKEN");
+    },
+
+    setUser: (state, user) => {
+      state.user.data = user;
+    },
+    setToken: (state, token) => {
+      state.user.token = token;
+      sessionStorage.setItem('TOKEN', token);
+    },
   },
   modules: {},
 })
